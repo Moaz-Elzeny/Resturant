d0b6d87df12361bdac53fc6f0d0373be76add85f
moaz <moazelzeny11@gmail.com>
2022-07-23 02:34:11 +0200

    mmmm

---
+++ Restaurant/Migrations/20220723002945_PhotoMigration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant.Migrations
+{
+    public partial class PhotoMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                schema: "dto",
+                table: "Order",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ItemPhoto",
+                schema: "dto",
+                table: "Item",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                schema: "dto",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "ItemPhoto",
+                schema: "dto",
+                table: "Item");
+        }
+    }
+}
